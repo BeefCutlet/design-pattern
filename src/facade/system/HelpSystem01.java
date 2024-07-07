b8c748c394d5697800b62dba450978e93cce220b
OSU <whysabi492@naver.com>
2024-07-07 23:45:10 +0900

    Add Facade Pattern

---
+++ src/facade/system/HelpSystem01.java
@@ -0,0 +1,12 @@
+package facade.system;
+
+class HelpSystem01 {
+
+    public HelpSystem01() {
+        System.out.println("Call Constructor : " + getClass().getSimpleName());
+    }
+
+    public void process() {
+        System.out.println("Call Process : " + getClass().getSimpleName());
+    }
+}
